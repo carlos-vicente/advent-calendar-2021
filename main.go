b5cf91778ec1932bca7d7ebc6944753933c672f6
Carlos Vicente <carlosvicente200@gmail.com>
2021-12-03 18:22:43 +0000

    day 2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"regexp"
 )
 
 func main() {
@@ -15,11 +16,23 @@ func main() {
 	// }
 	// fmt.Printf("Day 1 Challenge 1: %d\n", d1c1)
 
-	d1c2, err := day1Challenge2()
+	// d1c2, err := day1Challenge2()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Day 1 Challenge 2: %d\n", d1c2)
+
+	// d2c1, err := day2Challenge1()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Day 2 Challenge 1: %d\n", d2c1)
+
+	d2c2, err := day2Challenge2()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Day 1 Challenge 2: %d\n", d1c2)
+	fmt.Printf("Day 2 Challenge 2: %d\n", d2c2)
 }
 
 func readLines(path string) ([]int, error) {
@@ -100,4 +113,90 @@ func day1Challenge2() (int, error) {
 	}
 
 	return counter, nil
+}
+
+
+func day2Challenge1() (int, error) {
+	file, err := os.Open("./inputs/day_2.txt")
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	horizontal := 0
+	depth := 0
+
+	reg := regexp.MustCompile(`(?P<command>\w*)\s(?P<value>\d*)`)
+
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := reg.FindStringSubmatch(line)
+
+		switch command := matches[1]; command {
+		case "forward":
+			value, _ := strconv.Atoi(matches[2])
+			horizontal += value
+			fmt.Println("f", horizontal)
+		case "up":
+			value, _ := strconv.Atoi(matches[2])
+			depth -= value
+			fmt.Println("u", depth)
+		case "down":
+			value, _ := strconv.Atoi(matches[2])
+			depth += value
+			fmt.Println("d", depth)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return horizontal * depth, nil
+}
+
+
+func day2Challenge2() (int, error) {
+	file, err := os.Open("./inputs/day_2.txt")
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	horizontal := 0
+	depth := 0
+	aim := 0
+
+	reg := regexp.MustCompile(`(?P<command>\w*)\s(?P<value>\d*)`)
+
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := reg.FindStringSubmatch(line)
+
+		switch command := matches[1]; command {
+		case "forward":
+			value, _ := strconv.Atoi(matches[2])
+			horizontal += value
+			depth += aim * value
+			fmt.Printf("f %d %d\n", horizontal, depth)
+		case "up":
+			value, _ := strconv.Atoi(matches[2])
+			aim -= value
+			fmt.Printf("u %d\n", aim)
+		case "down":
+			value, _ := strconv.Atoi(matches[2])
+			aim += value
+			fmt.Printf("d %d\n", aim)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return horizontal * depth, nil
 }
